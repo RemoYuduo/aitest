12f70ba8aeb5779a5685ad7c5de635b3c8c199a9
Remo Yu <remoyu@qq.com>
2025-04-21 22:10:12 +0800

    11

--- game.js
+++ game.js
@@ -6,7 +6,8 @@ const ctx = canvas.getContext('2d');
 const gridSize = 20; // 网格大小
 const tileCount = canvas.width / gridSize; // 网格数量
 
-// 蛇的初始配置
+
+// 蛇的初始配置   注释
 let snake = {
     x: 10,
     y: 10,
